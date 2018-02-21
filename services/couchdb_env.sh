9dd236e26894c12d8c2c83229e3dea5c1eff815d
Riturajcse <riturajcse1994@gmail.com>
2018-02-21 11:35:32 +0530

    adds scripts for couchdb installation on c7all image

---
+++ services/couchdb_env.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash -e
+# Begin service ENV variables
+if [ -z "$SHIPPABLE_COUCHDB_PORT" ]; then
+  export SHIPPABLE_COUCHDB_PORT=5984;
+fi
+
+if [ -z "$SHIPPABLE_COUCHDB_BINARY" ]; then
+  export SHIPPABLE_COUCHDB_BINARY="/opt/couchdb/bin/couchdb";
+fi
+
+if [ -z "$SHIPPABLE_COUCHDB_CMD" ]; then
+  export SHIPPABLE_COUCHDB_CMD="service couchdb";
+fi
+
+if [ -z "$SHIPPABLE_COUCHDB_LOG" ]; then
+  export SHIPPABLE_COUCHDB_LOG="couchdb.stderr";
+fi
+
+# End service ENV variables
