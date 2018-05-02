e1c19c55365f2ca5fd5adc035e4d3a3655f4d389
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-05-02 18:47:04 +0530

    initialising the database

--- version/postgres_10.sh
+++ version/postgres_10.sh
@@ -3,4 +3,5 @@ POSTGRES_VERSION=10
 echo "================= Installing Postgres $POSTGRES_VERSION ==================="
 sudo yum install https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-centos10-10-2.noarch.rpm
 sudo yum install -y postgresql10 \
-   postgresql10-server
+  postgresql10-server
+sudo -u postgres /usr/pgsql-10/bin/initdb /var/lib/pgsql/data/
