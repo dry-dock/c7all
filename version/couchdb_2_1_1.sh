6816a5d7c582f7547da188728446e2c8237b8a12
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-05-18 15:13:00 +0530

    Update couchdb_2_1_1.sh

--- version/couchdb_2_1_1.sh
+++ version/couchdb_2_1_1.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
-echo "================= Installing Cassandra 2.1.1 ==================="
+echo "================= Installing Couchdb 2.1.1 ==================="
 echo "[bintray--apache-couchdb-rpm]
 name=bintray--apache-couchdb-rpm
 baseurl=http://apache.bintray.com/couchdb-rpm/el7/x86_64/
 gpgcheck=0
 repo_gpgcheck=0
 enabled=1" > /etc/yum.repos.d/bintray-apache-couchdb-rpm.repo
+mv /usr/lib/python2.7/site-packages/urllib3/packages/ssl_match_hostname  /usr/lib/python2.7/site-packages/urllib3/packages/ssl_match_hostname.backup
 sudo yum -y install couchdb
