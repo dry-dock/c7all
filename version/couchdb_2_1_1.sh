a67ec233d4d7f85d8f7e4585c4d189fd68fb5fe8
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-05-18 15:31:57 +0530

    Update couchdb_2_1_1.sh

--- version/couchdb_2_1_1.sh
+++ version/couchdb_2_1_1.sh
@@ -6,6 +6,7 @@ baseurl=http://apache.bintray.com/couchdb-rpm/el7/x86_64/
 gpgcheck=0
 repo_gpgcheck=0
 enabled=1" > /etc/yum.repos.d/bintray-apache-couchdb-rpm.repo
-#couchbd is failing if its on this path so moving it to other location 
+# couchbd is failing if its on this path so moving it to other location 
+# https://www.spinics.net/lists/centos/msg169867.html
 mv /usr/lib/python2.7/site-packages/urllib3/packages/ssl_match_hostname  /usr/lib/python2.7/site-packages/urllib3/packages/ssl_match_hostname.backup
 sudo yum -y install couchdb
