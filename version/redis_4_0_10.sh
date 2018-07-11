4e6d257f9a64467052e1d2dc3d03f458fb889c9b
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-07-11 13:03:43 +0530

    Update and rename redis_4_0.sh to redis_4_0_10.sh

--- version/redis_4_0_10.sh
+++ version/redis_4_0_10.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-REDIS_VER=4.0.9
+REDIS_VER=4.0.10
 echo "================= Installing redis-server ==================="
 wget http://download.redis.io/releases/redis-$REDIS_VER.tar.gz
 tar xzvf redis-$REDIS_VER.tar.gz
