4e96adb3c151b6716c319dce288d6f7afd76b069
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2019-01-04 11:36:31 +0530

    Update selenium_3_141_59.sh

--- version/selenium_3_141_59.sh
+++ version/selenium_3_141_59.sh
@@ -27,6 +27,7 @@ echo "================= Installing Opera ==================="
 wget http://get.geo.opera.com/pub/opera/linux/1216/opera-12.16-1860.x86_64.rpm
 sudo yum install -y opera-12.16-1860.x86_64.rpm
 
+export SELENIUM_VERSION=3.141.59
 echo "================= Installing Selenium ==================="
 sudo mkdir /usr/local/selenium >/dev/null 2>&1
-sudo wget http://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar -P /usr/local/selenium
+sudo wget http://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-"$SELENIUM_VERSION".jar -P /usr/local/selenium
