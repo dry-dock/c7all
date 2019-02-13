bd5d5ab83eeadfcb7baf0dbe14ab78509c196f32
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2019-02-13 14:44:18 +0530

    Update sqlite_3_27_1.sh

--- version/sqlite_3_27_1.sh
+++ version/sqlite_3_27_1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 SQLITE_VER=3.27.1
 echo "================= Installing sqlite $SQLITE_VER ==================="
-wget http://sqlite.org/2018/sqlite-autoconf-3220000.tar.gz
-tar xvfz sqlite-autoconf-3220000.tar.gz
-cd sqlite-autoconf-3220000
+wget https://sqlite.org/2019/sqlite-autoconf-3270100.tar.gz
+tar xvfz sqlite-autoconf-3270100.tar.gz
+cd sqlite-autoconf-3270100
 ./configure
 make
 make install
