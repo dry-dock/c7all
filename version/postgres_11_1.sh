4ab5901395b1c972cebfc706d66f93432528e85b
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2019-02-14 18:19:42 +0530

    Update postgres_11_1.sh

--- version/postgres_11_1.sh
+++ version/postgres_11_1.sh
@@ -4,5 +4,4 @@ echo "================= Installing Postgres $POSTGRES_VERSION ==================
 sudo yum install https://download.postgresql.org/pub/repos/yum/11/redhat/rhel-7-x86_64/pgdg-centos11-11-2.noarch.rpm
 sudo yum install -y postgresql11 \
    postgresql11-server
-#sudo -u postgres /usr/pgsql-11/bin/postgresql-11-setup initdb /var/lib/pgsql/data/   
 sudo -u postgres /usr/pgsql-11/bin/initdb /var/lib/pgsql/data/
