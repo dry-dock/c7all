23afa878beb85a55e4885c51dcdfc3da376a4a4a
vijayreddy1991 <vijayreddy.chenna@gmail.com>
2019-02-14 12:17:32 +0000

    adding missed version in env

--- services/postgres_env.sh
+++ services/postgres_env.sh
@@ -18,7 +18,7 @@ if [ -z "$SHIPPABLE_POSTGRES_BINARY" ]; then
 fi
 
 if [ -z "$SHIPPABLE_POSTGRES_CMD" ]; then
-  export SHIPPABLE_POSTGRES_CMD="sudo -u postgres /usr/pgsql-10/bin/pg_ctl -D /var/lib/pgsql/data start";
+  export SHIPPABLE_POSTGRES_CMD="sudo -u postgres /usr/pgsql-11/bin/pg_ctl -D /var/lib/pgsql/data start";
 fi
 
 # End service ENV variables
